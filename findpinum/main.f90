5db05a916e3efeb05a41773087457a80e2a86ce4
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-09-25 11:26:58 +0300

    Add findpi num with various parallel ways project

---
+++ findpinum/main.f90
@@ -0,0 +1,91 @@
+program main
+    use omp_lib
+    use module
+
+    real(precision) :: local_s
+
+    s = 0.0
+    t1 = omp_get_wtime()
+
+    do k = 0, NMAX - 1
+        s = s + (-1.0)**k / (2*k + 1)
+    end do
+
+    pi = 4.0 * s
+    t2 = omp_get_wtime()
+
+    write (*, *) "DO"
+    write (*, *) "PI   =", pi
+    write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
+
+    ! ===================== !
+
+    s = 0.0
+    t1 = omp_get_wtime()
+
+    do concurrent (i = 1:NPARTS)
+        part(i) = series(i)
+    end do
+    s = sum(part)
+
+    pi = 4.0 * s
+
+    t2 = omp_get_wtime()
+
+    write (*, *) "DO CONCURRENT"
+    write (*, *) "PI = ", pi
+    write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
+
+    ! ===================== !
+
+    s = 0.0
+    t1 = omp_get_wtime()
+
+    !$omp parallel do reduction(+:s)
+    do k = 0, NMAX - 1
+        s = s + (-1.0)**k / (2*k + 1)
+    end do
+    !$omp end parallel do
+
+    pi = 4.0 * s
+    t2 = omp_get_wtime()
+
+    write (*, *) "OPENMP"
+    write (*, *) "PI   =", pi
+    write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
+
+    ! ===================== !
+
+    s = 0.0
+    t1 = omp_get_wtime()
+
+    !$omp parallel private(k, local_s)
+
+    local_s = 0.0
+
+    !$omp do
+    do k = 0, NMAX - 1
+        local_s = local_s + (-1.0)**k / (2*k + 1)
+    end do
+    !$omp end do
+
+    !$omp critical
+    s = s + local_s
+    !$omp end critical
+
+    !$omp end parallel
+
+    pi = 4.0 * s
+    t2 = omp_get_wtime()
+
+    write (*, *) "OPENMP CRITICAL"
+    write (*, *) "PI   =", pi
+    write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
+
+    write (*, *) "THREADS =", omp_get_max_threads()
+
+end program main
---
+++ findpinum/module.f90
@@ -0,0 +1,28 @@
+module module
+    implicit none
+
+    integer, parameter :: NMAX = 500000000
+    integer, parameter :: NPARTS = 1000
+    integer, parameter :: precision = 8
+    integer :: i, k
+
+    real(precision) :: pi, s
+    real(precision) :: t1, t2
+    doubleprecision :: part(NPARTS)
+
+    contains
+        pure double precision function series(i)
+
+        integer, intent(in) :: i
+        integer :: k, first, last
+
+        first = (i - 1) * (NMAX / NPARTS)
+        last  = i * (NMAX / NPARTS) - 1
+
+        series = 0.0
+
+        do k = first, last
+            series = series + (-1)**k / dble(2*k + 1)
+        end do
+        end function series
+end module module
