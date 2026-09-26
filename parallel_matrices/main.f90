b3dc49fa2cfef611e5393065a488e1423c7e163a
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-09-26 16:21:36 +0300

    refactor: put do concurrent and openmp to logical order

--- parallel_matrices/main.f90
+++ parallel_matrices/main.f90
@@ -10,6 +10,7 @@ program parallel_matrices
     write (*, '(A, F10.3)') "A = ", A(1,1)
     write (*, '(A, F10.3)') "B = ", B(1,1)
     write (*, '(A, F10.3)') "C = ", C(1,1)
+    write (*, *)
 
     t1 = omp_get_wtime()
 
@@ -26,6 +27,8 @@ program parallel_matrices
     write (*, *) "DO"
     write (*, '(A, F10.3)') "C(1,1) = ", C(1,1)
     write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
+
 
     ! ===================== !
 
@@ -45,6 +48,29 @@ program parallel_matrices
     write (*, *) "DO CONCURRENT внешний"
     write (*, '(A, F10.3)') "C(1,1) = ", C(1,1)
     write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
+
+    ! ===================== !
+
+    C = 0.0
+    t1 = omp_get_wtime()
+
+    !$omp parallel do private(j, k)
+    do i = 1, N
+        do j = 1, N
+            do k = 1, N
+                C(i,j) = C(i,j) + A(i,k) * B(k,j)
+            end do
+        end do
+    end do
+    !$omp end parallel do
+
+    t2 = omp_get_wtime()
+
+    write (*, *) "OPENMP внешний"
+    write (*, '(A, F10.3)') "C(1,1) = ", C(1,1)
+    write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
 
     ! ===================== !
 
@@ -74,6 +100,30 @@ program parallel_matrices
     t1 = omp_get_wtime()
 
     do i = 1, N
+
+        !$omp parallel do private(k)
+        do j = 1, N
+            do k = 1, N
+                C(i,j) = C(i,j) + A(i,k) * B(k,j)
+            end do
+        end do
+        !$omp end parallel do
+
+    end do
+
+    t2 = omp_get_wtime()
+
+    write (*, *) "OPENMP средний"
+    write (*, '(A, F10.3)') "C(1,1) = ", C(1,1)
+    write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
+
+    ! ===================== !
+
+    C = 0.0
+    t1 = omp_get_wtime()
+
+    do i = 1, N
         do j = 1, N
 
             do concurrent (k = 1:N)
@@ -92,7 +142,32 @@ program parallel_matrices
     write (*, '(A, F10.5)') "TIME = ", t2 - t1
     write (*, *)
 
+    ! ===================== !
 
+    C = 0.0
+    t1 = omp_get_wtime()
 
+    do i = 1, N
+        do j = 1, N
+
+            s = 0.0
+
+            !$omp parallel do reduction(+:s)
+            do k = 1, N
+                s = s + A(i,k) * B(k,j)
+            end do
+            !$omp end parallel do
+
+            C(i,j) = s
+
+        end do
+    end do
+
+    t2 = omp_get_wtime()
+
+    write (*, *) "OPENMP внутренний"
+    write (*, '(A, F10.3)') "C(1,1) = ", C(1,1)
+    write (*, '(A, F10.5)') "TIME = ", t2 - t1
+    write (*, *)
 
 end program
